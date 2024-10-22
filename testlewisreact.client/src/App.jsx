78646b49e07675ba3c1e1edde5d9c864bdd2965c
Lewis Foot <lfoot91@hotmail.co.uk>
2024-10-22 20:28:58 +0100

    fix: still not displaying the list returned from the API correctly

--- testlewisreact.client/src/App.jsx
+++ testlewisreact.client/src/App.jsx
@@ -10,7 +10,13 @@ function App() {
 
     const contents = exams === undefined
         ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <p>exams.ExamName</p>
+        : (
+            <ul>
+                {exams.map((exam, index) => (
+                    <li key={index}>{exam.ExamName}</li>
+                ))}
+            </ul>
+        )
 
     return (
         <div>
