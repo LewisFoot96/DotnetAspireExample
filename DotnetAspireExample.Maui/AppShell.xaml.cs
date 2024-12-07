aaffd4c3eed4e7c07e1fbbb48744e5f736501eb1
Lewis Foot <lfoot91@hotmail.co.uk>
2024-12-07 11:46:39 +0000

    feat: added example .NET MAUI project

---
+++ DotnetAspireExample.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetAspireExample.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DotnetAspireExample.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DotnetAspireExample.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
